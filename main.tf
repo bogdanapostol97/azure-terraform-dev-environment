6c166f21adf68fa0c86de8d99fd9e6c1468d0fff
Bogdan Apostol <40504635+bogdanapostol97@users.noreply.github.com>
2024-11-25 16:56:00 +0200

    Final version of the project

---
+++ main.tf
@@ -0,0 +1,145 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "prefix" {
+  type    = string
+  default = "mtc"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
+variable "vm_size" {
+  type    = string
+  default = "Standard_B1s"
+}
+
+resource "azurerm_resource_group" "mtc-rg" {
+  name     = "${var.prefix}-resources"
+  location = "West Europe"
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+resource "azurerm_virtual_network" "mtc-vn" {
+  name                = "${var.prefix}-network"
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+  location            = azurerm_resource_group.mtc-rg.location
+  address_space       = ["10.123.0.0/16"]
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+resource "azurerm_subnet" "mtc-subnet" {
+  name                 = "${var.prefix}-subnet"
+  resource_group_name  = azurerm_resource_group.mtc-rg.name
+  virtual_network_name = azurerm_virtual_network.mtc-vn.name
+  address_prefixes     = ["10.123.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "mtc-sg" {
+  name                = "${var.prefix}-sg"
+  location            = azurerm_resource_group.mtc-rg.location
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+
+resource "azurerm_network_security_rule" "mtc-dev-rule" {
+  name                        = "${var.prefix}-dev-rule"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.mtc-rg.name
+  network_security_group_name = azurerm_network_security_group.mtc-sg.name
+}
+
+
+resource "azurerm_subnet_network_security_group_association" "mtc-sga" {
+  subnet_id                 = azurerm_subnet.mtc-subnet.id
+  network_security_group_id = azurerm_network_security_group.mtc-sg.id
+
+}
+
+
+resource "azurerm_public_ip" "mtc-ip" {
+  name                = "${var.prefix}-ip"
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+  location            = azurerm_resource_group.mtc-rg.location
+  allocation_method   = "Dynamic"
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+resource "azurerm_network_interface" "mtc-nic" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.mtc-rg.location
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.mtc-subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.mtc-ip.id
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+//se poate folosi name = "${var.prefix}-vm", unde var este o variabila globala declarata la inceput; valabil si pt celelalte resurse
+resource "azurerm_linux_virtual_machine" "mtc-vm" {
+  name                  = "${var.prefix}-vm"
+  resource_group_name   = azurerm_resource_group.mtc-rg.name
+  location              = azurerm_resource_group.mtc-rg.location
+  size                  = var.vm_size
+  admin_username        = "adminuser"
+  network_interface_ids = [azurerm_network_interface.mtc-nic.id]
+
+  custom_data = filebase64("customdata.tpl")
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/mtcazurekey.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+}
